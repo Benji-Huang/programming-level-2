b356d3515bedb4e6c270fa4c11bf09874fa7a8a0
“Benjamin <benjihuang03@gmail.com>
2021-01-19 11:41:10 -0800

    v1.0

--- pygame/shmup.py
+++ pygame/shmup.py
@@ -2,26 +2,34 @@
 #get practice with OOP and pygame
 
 import pygame
+import random
 
 # ----- CONSTANTS
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 SKY_BLUE = (95, 165, 228)
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 720
+HEIGHT = 1000
 TITLE = "SHMUP"
+NUM_ROWS = 8
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
         self.image = pygame.image.load("./images/galaga_ship.png")
+        # Scaling down the image x0.5
+        self.image = pygame.transform.scale(self.image, (64,64))
+
         self.rect = self.image.get_rect()
 
     def update(self):
         """Move the player with a mouse"""
         self.rect.center = pygame.mouse.get_pos()
+
+        if self.rect.top < HEIGHT - 100:
+            self.rect.top = HEIGHT - 100
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, y_coord):
@@ -49,6 +57,26 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.right > WIDTH or self.rect.left < 0:
             self.x_vel *= -1
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, coords):
+        """
+        Arguments:
+            coords - tuple of x,y
+        """
+        super().__init__()
+
+        self.image = pygame.image.load("./images/bullet.png")
+        # scale to 22x36px
+        self.image = pygame.transform.scale(self.image, (22,36))
+
+        self.rect = self.image.get_rect()
+        # start at coords
+        self.rect.centerx, self.rect.bottom = coords
+
+        self.y_vel = -3
+
+    def update(self):
+        self.rect.y += self.y_vel
 
 def main():
     pygame.init()
@@ -65,11 +93,14 @@ def main():
     # Sprite Groups
     all_sprites = pygame.sprite.RenderUpdates()
     enemy_sprites = pygame.sprite.Group()
+    player_bullet_sprites = pygame.sprite.Group()
 
     #  --- enemies
-    enemy = Enemy(100)
-    all_sprites.add(enemy)
-    enemy_sprites.add(enemy)
+    for i in range(NUM_ROWS):
+        enemy = Enemy(100+i*30)
+        enemy.rect.x = enemy.rect.x - random.choice([-10, 10])
+        all_sprites.add(enemy)
+        enemy_sprites.add(enemy)
 
     #  --- player
     player = Player()
@@ -81,9 +112,24 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if len(player_bullet_sprites) < 3:
+                # create a bullet
+                    bullet = Bullet(player.rect.midtop)
+                    all_sprites.add(bullet)
+                    player_bullet_sprites.add(bullet)
 
         # ----- LOGIC
         all_sprites.update()
+
+        # check if every bullet has collided with an enemy
+        for bullet in player_bullet_sprites:
+            # kill if off screen
+            if bullet.rect.bottom < 0:
+                bullet.kill()
+            enemy_hit_group = pygame.sprite.spritecollide(bullet, enemy_sprites, True)
+            if len(enemy_hit_group) > 0:
+                bullet.kill()
 
         # ----- DRAW
         screen.fill(BLACK)
